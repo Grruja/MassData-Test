af4afdeaca2eb6c592f2dd32f3aa83d6880d9084
AleexxG <alexanderg.sln@gmail.com>
2024-07-31 00:22:52 +0200

    Relation added to user and user permission

--- app/Models/UserPermission.php
+++ app/Models/UserPermission.php
@@ -3,10 +3,16 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class UserPermission extends Model
 {
     protected $table = 'user_permissions';
 
-    protected $fillable = ['user_id', 'import_permission'];
+    protected $fillable = ['user_id', 'permission_id'];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
