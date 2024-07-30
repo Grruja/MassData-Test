3fb73b58948bfdd8a5505d489355b5aaf97c43e4
AleexxG <alexanderg.sln@gmail.com>
2024-07-30 11:19:06 +0200

    Set blade layout with adminlte page

---
+++ resources/views/layouts/adminlte.blade.php
@@ -0,0 +1,25 @@
+@extends('adminlte::page')
+
+@section('title')
+    {{ config('adminlte.title') }}
+    @hasSection('subtitle') | @yield('subtitle') @endif
+@stop
+
+@section('content_header')
+    @hasSection('content_header_title')
+        <h1 class="text-muted">
+            @yield('content_header_title')
+
+            @hasSection('content_header_subtitle')
+                <small class="text-dark">
+                    <i class="fas fa-xs fa-angle-right text-muted"></i>
+                    @yield('content_header_subtitle')
+                </small>
+            @endif
+        </h1>
+    @endif
+@stop
+
+@section('content')
+    @yield('content_body')
+@stop
