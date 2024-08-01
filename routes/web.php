8a3dfd0f5ec31d4314d237e05596ef04444da7e3
AleexxG <alexanderg.sln@gmail.com>
2024-08-01 15:11:52 +0200

    Added group route with auth middleware

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,10 @@
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome');
-Route::view('/home', 'home')->name('home');
+
+Route::middleware('auth')->group(function () {
+    Route::view('/home', 'home')->name('home');
+});
 
 Auth::routes();
 
