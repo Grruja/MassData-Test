a249e88f9f98784f36617e39960a80849c273c72
AleexxG <alexanderg.sln@gmail.com>
2024-08-01 15:09:21 +0200

    User seeder added to create admin and fake users

--- database/migrations/2024_07_30_216550_create_user_permissions_table.php
+++ database/migrations/2024_07_30_216550_create_user_permissions_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -20,6 +21,10 @@ return new class extends Migration
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->foreign('permission_id')->references('id')->on('permissions')->onDelete('cascade');
         });
+
+        Artisan::call('db:seed', [
+            '--class' => 'UserSeeder',
+        ]);
     }
 
     /**
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Permission;
+use App\Models\User;
+use Faker\Factory;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $admin = User::create([
+            'name' => 'Admin',
+            'email' => 'admin@gmail.com',
+            'password' => Hash::make('123456'),
+        ]);
+
+        $permission = Permission::firstWhere('name', 'user-management');
+        $admin->userPermission()->create(['permission_id' => $permission->id]);
+
+        $faker = Factory::create();
+
+        for ($i = 0; $i < 10; $i++) {
+            User::create([
+                'name' => $faker->name,
+                'email' => $faker->email,
+                'password' => Hash::make('123456'),
+            ]);
+        }
+    }
+}
