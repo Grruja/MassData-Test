a3033ab1b4e740c6624ab8ce93a3a211da8ff9f6
AleexxG <alexanderg.sln@gmail.com>
2024-08-02 12:30:27 +0200

    Content change

--- resources/views/permissions/create.blade.php
+++ resources/views/permissions/create.blade.php
@@ -1,7 +1,7 @@
 <div class="mb-5 mt-3 border-bottom pb-5">
     <form action="{{ route('permissions.create') }}" method="POST" style="max-width: 350px">
         @csrf
-        <legend class="text-lg mb-4">Create new permission</legend>
+        <legend class="text-lg mb-4">Create New Permission</legend>
         <x-adminlte-input name="name" placeholder="permission name" required>
             <x-slot name="appendSlot">
                 <x-adminlte-button type="submit" label="Create" theme="primary" />
