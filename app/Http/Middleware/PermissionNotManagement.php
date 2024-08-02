ea89872888e3f7d1790b8adf0c5662bf9a8eec07
AleexxG <alexanderg.sln@gmail.com>
2024-08-03 00:41:38 +0200

    File name changed

--- app/Http/Middleware/PermissionNotManagement.php
+++ app/Http/Middleware/PermissionNotManagement.php
@@ -2,11 +2,12 @@
 
 namespace App\Http\Middleware;
 
+use App\Models\Permission;
 use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 
-class CheckPermissionParameter
+class PermissionNotManagement
 {
     /**
      * Handle an incoming request.
@@ -17,7 +18,9 @@ class CheckPermissionParameter
     {
         $permission = $request->route('permission');
 
-        if ($permission->id == 1) return redirect()->back();
+        if ($permission->id == Permission::getPermissionId('user-management')) {
+            abort(403);
+        }
 
         return $next($request);
     }
