114d581cd0efa00ef68a3be189ebd1be40645834
AleexxG <alexanderg.sln@gmail.com>
2024-08-03 23:44:06 +0200

    Message dump

--- app/Http/Controllers/ImportController.php
+++ app/Http/Controllers/ImportController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreImporetedDataRequest;
+use App\Imports\OrderImport;
 use App\Services\ImportService;
 use Illuminate\View\View;
+use Maatwebsite\Excel\Facades\Excel;
 
 class ImportController extends Controller
 {
@@ -18,6 +20,10 @@ class ImportController extends Controller
 
     public function storeImportedData(StoreImporetedDataRequest $request)
     {
-        dd('passed');
+        dd('Validation passed');
+
+        /*foreach ($request->file('files') as $file) {
+            Excel::import(new OrderImport, $file);
+        }*/
     }
 }
