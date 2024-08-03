2987048ffa093f0dd0d7c22953870f1c7e83e328
AleexxG <alexanderg.sln@gmail.com>
2024-08-03 13:23:18 +0200

    Added relation

--- app/Models/Permission.php
+++ app/Models/Permission.php
@@ -4,12 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Permission extends Model
 {
     protected $table = 'permissions';
 
     protected $fillable = ['name'];
+
+    public function userPermissions(): HasMany
+    {
+        return $this->hasMany(UserPermission::class);
+    }
 
     public static function getPermissionId(string $permissionName): ?int
     {
--- app/Models/UserPermission.php
+++ app/Models/UserPermission.php
@@ -15,4 +15,9 @@ class UserPermission extends Model
     {
         return $this->belongsTo(User::class);
     }
+
+    public function permission(): BelongsTo
+    {
+        return $this->belongsTo(Permission::class);
+    }
 }
